33f80220002f6ceef5ffc0adc2f3c40feec8bd00
Roman Subik <roman.subik@ocado.com>
2018-10-14 08:44:44 +0200

    Initialize spring project.

---
+++ src/main/java/rjm/romek/awscourse/AwsCourseApplication.java
@@ -0,0 +1,11 @@
+package rjm.romek.awscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCourseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCourseApplication.class, args);
+	}
+}
