3ed7bf47033047709be43783af41a88c7e3356ea
Roman Subik <roman.subik@ocado.com>
2018-11-03 12:32:30 +0100

    Load users from file.

---
+++ src/main/java/rjm/romek/awscourse/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package rjm.romek.awscourse;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+
+    @Value("${credentials.path}")
+    private String credentialsPath;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(loadUsers());
+    }
+
+    private UserDetails [] loadUsers() {
+        List<String> credentials = null;
+        List<UserDetails> userDetails = new ArrayList<UserDetails>();
+
+        try {
+            File file = new ClassPathResource(credentialsPath).getFile();
+            credentials = FileUtils.readLines(file, "UTF8");
+            for (String userData : credentials) {
+                String [] userDataSplited = userData.split(",");
+                String userName = userDataSplited[0];
+                String password = userDataSplited[1];
+                String authority = userDataSplited[2];
+                userDetails.add(
+                        User.withDefaultPasswordEncoder()
+                                .username(userName)
+                                .password(password)
+                                .authorities(authority)
+                                .build()
+                );
+            }
+        } catch (IOException e) {
+            logger.error("Could not read users. Exception: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return userDetails.toArray(new UserDetails[userDetails.size()]);
+    }
+}
--- src/main/java/rjm/romek/awscourse/controller/S3ChapterControler.java
+++ src/main/java/rjm/romek/awscourse/controller/S3ChapterControler.java
@@ -11,7 +11,7 @@ public class S3ChapterControler {
     @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "index";
+        return "hello";
     }
 
 }
