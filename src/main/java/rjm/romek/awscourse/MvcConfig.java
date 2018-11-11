17ded004bf4b46f96596671bc0fa1f07cf1dc86f
Roman Subik <roman.subik@ocado.com>
2018-11-11 07:31:07 +0100

    Add jQuery/bootstrap via webjars.

--- src/main/java/rjm/romek/awscourse/MvcConfig.java
+++ src/main/java/rjm/romek/awscourse/MvcConfig.java
@@ -1,6 +1,7 @@
 package rjm.romek.awscourse;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,13 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
     }
 
 }
