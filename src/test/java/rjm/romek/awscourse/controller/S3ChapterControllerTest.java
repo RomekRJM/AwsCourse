ea7e254fdfd1178a5c8702e4d35be25c18de0473
Roman Subik <roman.subik@ocado.com>
2018-11-12 15:28:35 +0100

    Fix failing tests.

--- src/test/java/rjm/romek/awscourse/controller/S3ChapterControllerTest.java
+++ src/test/java/rjm/romek/awscourse/controller/S3ChapterControllerTest.java
@@ -1,38 +1,52 @@
 package rjm.romek.awscourse.controller;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Collections;
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import rjm.romek.awscourse.model.Chapter;
 import rjm.romek.awscourse.repository.ChapterRepository;
+import rjm.romek.awscourse.repository.TaskRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc(secure=false)
+@WebMvcTest(S3ChapterController.class)
+@AutoConfigureMockMvc
 public class S3ChapterControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private ChapterRepository chapterRepository;
 
-    @Test
-    public void shouldGetChapterAndTasks() throws Exception {
-        chapterRepository.save(new Chapter("Test"));
+    @MockBean
+    private TaskRepository taskRepository;
 
-        mockMvc.perform(get(S3ChapterController.PATH))
+    @Test
+    @WithUserDetails("tester")
+    public void shouldGetChapterAndTasks() throws Exception {
+        Chapter testChapter = new Chapter("Test");
+        when(chapterRepository.findById(anyLong())).thenReturn(Optional.of(testChapter));
+        when(taskRepository.findByChapter(testChapter)).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/" + S3ChapterController.PATH))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().string(containsString("Chapter Test")));
