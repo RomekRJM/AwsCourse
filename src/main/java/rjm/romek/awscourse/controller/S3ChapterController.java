284d2def4d5cac70e30cac71f217de9b794b2b64
Roman Subik <roman.subik@ocado.com>
2018-11-20 23:23:05 +0100

    Improve form functionality.

--- src/main/java/rjm/romek/awscourse/controller/S3ChapterController.java
+++ src/main/java/rjm/romek/awscourse/controller/S3ChapterController.java
@@ -1,15 +1,16 @@
 package rjm.romek.awscourse.controller;
 
 import java.util.List;
+import java.util.Map;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,9 +24,11 @@ import rjm.romek.awscourse.repository.TaskRepository;
 public class S3ChapterController {
 
     public static final String PATH = "chapter";
+    public static final String NEXT = "next";
     public static final String CHAPTER = "chapter";
     public static final String TASKS = "tasks";
     public static final String CHAPTER_ID = "chapterId";
+    public static final String ANSWER = "answer";
 
     @Autowired
     private ChapterRepository chapterRepository;
@@ -33,27 +36,30 @@ public class S3ChapterController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @GetMapping({"/", "/chapter"})
-    public ModelAndView showForm() {
-        return new ModelAndView("chapter", "chapter", chapterRepository.findById(1l).get());
+    @GetMapping({"/", "/chapter/{id}"})
+    public ModelAndView showForm(@PathVariable Long id) {
+        return new ModelAndView(PATH, loadChapterAndTasks(id, ""));
     }
 
     @PostMapping({"/", "/chapter"})
-    public String chapter(@RequestParam(name=CHAPTER_ID, required=false, defaultValue="1") Long chapterId,
-                          @Valid @ModelAttribute("bucketName") String bucketName,
-                          BindingResult result,
+    public String chapter(HttpServletRequest request,
+                          @RequestParam(value="id", required=true) Long id,
+                          @RequestParam(value="answer", required=true) String answer,
                           Model model) {
-
-        if (result.hasErrors()) {
-            return "error";
-        }
-
-        Chapter chapter = chapterRepository.findById(chapterId).get();
-        List<Task> tasks = taskRepository.findByChapter(chapter);
-
-        model.addAttribute(CHAPTER, chapter);
-        model.addAttribute(TASKS, tasks);
+        model.addAllAttributes(loadChapterAndTasks(id, answer));
         return PATH;
     }
 
+    private Map<String, Object> loadChapterAndTasks(Long chapterId, String answer) {
+        Map<String, Object> modelMap = new ModelMap();
+
+        Chapter chapter = chapterRepository.findById(chapterId).get();
+        List<Task> tasks = taskRepository.findByChapter(chapter);
+        tasks.get(0).setAnswer(answer);
+
+        modelMap.put(CHAPTER, chapter);
+        modelMap.put(TASKS, tasks);
+
+        return modelMap;
+    }
 }
