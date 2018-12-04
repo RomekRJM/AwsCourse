9c7f657d4d8f01f62a12ceff5e7f844883390de1
Roman Subik <roman.subik@ocado.com>
2018-12-04 22:45:02 +0100

    Add additional user.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO user(user_id, username, password, enabled) VALUES(1000, 'tester', '$2a$12$RIHZWG9xp6GcM.iIOEPV..9/vPcQs6kqDKnMHjy6lFMqMDbCxOAqC', true);
+INSERT INTO user(user_id, username, password, enabled) VALUES(1001, 'pusiak', '$2a$12$RIHZWG9xp6GcM.iIOEPV..9/vPcQs6kqDKnMHjy6lFMqMDbCxOAqC', true);
 
 INSERT INTO chapter(chapter_id, title) VALUES(1000, 'S3');
 
