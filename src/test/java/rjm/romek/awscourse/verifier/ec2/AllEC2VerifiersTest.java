aac5ca6d9de93aa943f0bb6e5f64f94b50fd6ac7
Roman Subik <roman.subik@ocado.com>
2018-12-24 10:52:41 +0100

    Test all EC2 verifiers.

--- src/test/java/rjm/romek/awscourse/verifier/ec2/AllEC2VerifiersTest.java
+++ src/test/java/rjm/romek/awscourse/verifier/ec2/AllEC2VerifiersTest.java
@@ -22,6 +22,7 @@ import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.Tag;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
@@ -30,10 +31,10 @@ import rjm.romek.awscourse.testutils.TestUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class EC2TypeVerifierTest {
+public class AllEC2VerifiersTest {
 
-    private static final String TASK_DESCRIPTION = "Create an EC2 instance of type t2.micro and paste id in here: " +
-            "(instanceId).(*instanceType=m3.medium)";
+    private static final String TASK_DESCRIPTION = "(instanceId)(*vpcId=vpc-2032f54b)(*instanceType=m3.medium)" +
+            "(*subnetId=subnet-c08152a7)(*ami=ami-e6fc5e91)(*tags=app:awscourse)";
     private static final String INSTANCE_ID = "i-a123456789";
 
     private static final UserTask USER_TASK = TestUtils.createUserTask(
@@ -59,6 +60,18 @@ public class EC2TypeVerifierTest {
     @Autowired
     private EC2TypeVerifier ec2TypeVerifier;
 
+    @Autowired
+    private EC2AmiVerifier ec2AmiVerifier;
+
+    @Autowired
+    private EC2SubnetVerifier ec2SubnetVerifier;
+
+    @Autowired
+    private EC2TagsVerifier ec2TagsVerifier;
+
+    @Autowired
+    private EC2VpcVerifier ec2VpcVerifier;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -72,16 +85,72 @@ public class EC2TypeVerifierTest {
     }
 
     @Test
-    public void isCompletedShouldReturnTrue() {
+    public void isCompletedShouldReturnTrueOnCorrectType() {
         when(instance.getInstanceType()).thenReturn("m3.medium");
 
         assertTrue(ec2TypeVerifier.isCompleted(USER_TASK));
     }
 
     @Test
-    public void isCompletedShouldReturnFalseOnWrongInstanceType() {
+    public void isCompletedShouldReturnFalseOnWrongType() {
         when(instance.getInstanceType()).thenReturn("t2.nano");
 
         assertFalse(ec2TypeVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnTrueOnCorrectVpc() {
+        when(instance.getVpcId()).thenReturn("vpc-2032f54b");
+
+        assertTrue(ec2VpcVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnFalseOnWrongVpc() {
+        when(instance.getVpcId()).thenReturn("vpc-00000000");
+
+        assertFalse(ec2VpcVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnTrueOnCorrectAmi() {
+        when(instance.getImageId()).thenReturn("ami-e6fc5e91");
+
+        assertTrue(ec2AmiVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnFalseOnWrongAmi() {
+        when(instance.getImageId()).thenReturn("ami-00000000");
+
+        assertFalse(ec2AmiVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnTrueOnCorrectSubnet() {
+        when(instance.getSubnetId()).thenReturn("subnet-c08152a7");
+
+        assertTrue(ec2SubnetVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnFalseOnWrongSubnet() {
+        when(instance.getSubnetId()).thenReturn("subnet-00000000");
+
+        assertFalse(ec2SubnetVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnTrueOnCorrectTags() {
+        when(instance.getTags()).thenReturn(ImmutableList.of(new Tag("app", "awscourse")));
+
+        assertTrue(ec2TagsVerifier.isCompleted(USER_TASK));
+    }
+
+    @Test
+    public void isCompletedShouldReturnFalseOnWrongTags() {
+        when(instance.getTags()).thenReturn(ImmutableList.of(new Tag("wrong", "tag")));
+
+        assertFalse(ec2TagsVerifier.isCompleted(USER_TASK));
     }
 }
