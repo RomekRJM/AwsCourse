4d4ab93ace3c6ecee132c7066ad941f8cba3314b
Roman Subik <roman.subik@ocado.com>
2018-12-24 06:10:30 +0100

    Split ami verification to separate task.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,5 +23,9 @@ VALUES (1002,
 INSERT INTO chapter(chapter_id, title) VALUES (1001, 'EC2');
 
 INSERT INTO task (task_id, description, verifier, chapter_chapter_id)
-VALUES (1003, 'Create an EC2 instance using Amazon Linux AMI 2018.03.0 HVM - ami:"ami-08935252a36e25f85", type:"t2.micro", vpc:"vpc-2e32f54b | vpc-evl-ocd", subnet:"subnet-df8352a8 | rds-c-evl-ocd | eu-west-1c" and add tag:"key:app, value:awscourse". Paste instance id in here: (instanceId).(*instanceType=m3.medium)(*ami=ami-09693313102a30b2c)(*vpc=vpc-2e32f54b)(*subnet=subnet-df8352a8)(*tag=app:awscourse)',
+VALUES (1003, 'Create an EC2 instance using type:"t2.micro", vpc:"vpc-2e32f54b | vpc-evl-ocd", subnet:"subnet-df8352a8 | rds-c-evl-ocd | eu-west-1c" and add tag:"key:app, value:awscourse". Paste instance id in here: (instanceId).(*instanceType=m3.medium)(*ami=ami-09693313102a30b2c)(*vpc=vpc-2e32f54b)(*subnet=subnet-df8352a8)(*tag=app:awscourse)',
         'rjm.romek.awscourse.verifier.ec2.EC2TypeVerifier', 1001);
+
+INSERT INTO task (task_id, description, verifier, chapter_chapter_id)
+VALUES (1004, 'Use Amazon Linux AMI 2018.03.0 HVM ami:"ami-e6fc5e91".(*ami=ami-e6fc5e91)(*instanceId)',
+        'rjm.romek.awscourse.verifier.ec2.EC2AmiVerifier', 1001);
